163d27d96f54778a505a30d4a601281aeef90321
Rahimi <develop.rahimi95@gmail.com>
2019-01-21 14:26:47 +0330

    bug fixed

--- podchat/src/main/java/com/fanap/podchat/cachemodel/queue/FailedMessage.java
+++
@@ -1,7 +0,0 @@
-package com.fanap.podchat.cachemodel.queue;
-
-import android.arch.persistence.room.Entity;
-
-public class FailedMessage {
-
-}
--- podchat/src/main/java/com/fanap/podchat/cachemodel/queue/UploadingMessage.java
+++
@@ -1,9 +0,0 @@
-package com.fanap.podchat.cachemodel.queue;
-
-import android.arch.persistence.room.Entity;
-
-@Entity
-public class UploadingMessage {
-
-
-}
