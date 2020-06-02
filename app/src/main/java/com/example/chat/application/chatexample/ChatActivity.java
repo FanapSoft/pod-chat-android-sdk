5d9e16af4a8f70f01455341793da0610f6861938
f.kheirkhah <farhadkheirkhah7@gmail.com>
2020-06-02 23:24:14 +0430

    improve search contact

--- app/src/main/java/com/example/chat/application/chatexample/ChatActivity.java
+++ app/src/main/java/com/example/chat/application/chatexample/ChatActivity.java
@@ -202,25 +202,25 @@ public class ChatActivity extends AppCompatActivity
      * Main Server Setting:
      */
 //
-//    private static String name = BaseApplication.getInstance().getString(R.string.main_server_name);
-//    private static String socketAddress = BaseApplication.getInstance().getString(R.string.socketAddress);
-//    private static String platformHost = BaseApplication.getInstance().getString(R.string.platformHost);
-//    private static String fileServer = BaseApplication.getInstance().getString(R.string.fileServer);
+    private static String name = BaseApplication.getInstance().getString(R.string.main_server_name);
+    private static String socketAddress = BaseApplication.getInstance().getString(R.string.socketAddress);
+    private static String platformHost = BaseApplication.getInstance().getString(R.string.platformHost);
+    private static String fileServer = BaseApplication.getInstance().getString(R.string.fileServer);
 
     /**
      * Sandbox setting:
      */
 
-    private static String name = BaseApplication.getInstance().getString(R.string.sandbox_server_name);
-    private static String socketAddress = BaseApplication.getInstance().getString(R.string.sandbox_socketAddress);
-    private static String platformHost = BaseApplication.getInstance().getString(R.string.sandbox_platformHost);
-    private static String fileServer = BaseApplication.getInstance().getString(R.string.sandbox_fileServer);
+//    private static String name = BaseApplication.getInstance().getString(R.string.sandbox_server_name);
+//    private static String socketAddress = BaseApplication.getInstance().getString(R.string.sandbox_socketAddress);
+//    private static String platformHost = BaseApplication.getInstance().getString(R.string.sandbox_platformHost);
+//    private static String fileServer = BaseApplication.getInstance().getString(R.string.sandbox_fileServer);
 
 
 //    //sand box / group
 
-    public static int TEST_THREAD_ID = 5182;
-    private static final String TEST_THREAD_HASH = "X6NO3WJRWTUMN8";
+//    public static int TEST_THREAD_ID = 5182;
+//    private static final String TEST_THREAD_HASH = "X6NO3WJRWTUMN8";
 
 
 //    main server / p2p
@@ -230,8 +230,8 @@ public class ChatActivity extends AppCompatActivity
 
     // main server / group
 
-//    public static int TEST_THREAD_ID = 47528;
-//    private static final String TEST_THREAD_HASH = "4S5U1G4EH82BVB";
+    public static int TEST_THREAD_ID = 47528;
+    private static final String TEST_THREAD_HASH = "4S5U1G4EH82BVB";
 
 
 //    integration /group: fifi,jiji and ...
@@ -1391,12 +1391,12 @@ public class ChatActivity extends AppCompatActivity
                         break;
                     case 9:
                         RequestSearchContact requestSearchContact = new RequestSearchContact
-                                .Builder("0", "5")
+                                .Builder("0", "20")
 //                                .id("2247")
-//                                .cellphoneNumber("093")
+//                                .cellphoneNumber("0938")
 //                                .lastName("kHeI")
 //                                .firstName("fAr")
-//                                .query("Bakhsh")
+                                .query("pO")
 //                                .order("desc")
 //                                .email("masoudmanson@gmail.com")
                                 .build();
