bf3099ff28b227cf120f74c1cad50def31ff669f
f.kheirkhah <farhadkheirkhah7@gmail.com>
2020-06-28 11:46:25 +0430

    add call history

---
+++ podchat/src/main/java/com/fanap/podchat/chat/call/persist/CacheCall.java
@@ -0,0 +1,4 @@
+package com.fanap.podchat.chat.call.persist;
+
+public class CacheCall {
+}
---
+++ podchat/src/main/java/com/fanap/podchat/chat/call/result_model/CallReconnectResult.java
@@ -0,0 +1,4 @@
+package com.fanap.podchat.chat.call.result_model;
+
+public class CallReconnectResult {
+}
