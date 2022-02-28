3be1ea583b5444303929747919c2b6651c7ca0b4
fanap_soft_26 <ahmadsajadidev73@gmail.com>
2022-02-28 21:42:19 +0330

    add apis for define and getkey in encryption

---
+++ podchat/src/main/java/com/fanap/podchat/model/OutPutDefineSecretKey.java
@@ -0,0 +1,19 @@
+package com.fanap.podchat.model;
+
+public class OutPutDefineSecretKey  {
+    private String keyId;
+    private String algorithm;
+    private String clientId;
+
+    public String getKeyId() {
+        return keyId;
+    }
+
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+}
---
+++ podchat/src/main/java/com/fanap/podchat/model/OutPutGetKey.java
@@ -0,0 +1,34 @@
+package com.fanap.podchat.model;
+
+public class OutPutGetKey {
+    private String keyId;
+    private String algorithm;
+    private String clientId;
+    private String privateKey;
+    private String publicKey;
+    private String keyFormat;
+
+    public String getKeyId() {
+        return keyId;
+    }
+
+    public String getAlgorithm() {
+        return algorithm;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public String getPrivateKey() {
+        return privateKey;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public String getKeyFormat() {
+        return keyFormat;
+    }
+}
