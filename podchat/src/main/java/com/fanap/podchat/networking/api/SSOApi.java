4c798c9d82bd6a6a072368c7d80bd7e21670316c
fanap_soft_26 <ahmadsajadidev73@gmail.com>
2022-02-28 21:41:28 +0330

    add apis for define and getkey in encryption

--- podchat/src/main/java/com/fanap/podchat/networking/api/SSOApi.java
+++ podchat/src/main/java/com/fanap/podchat/networking/api/SSOApi.java
@@ -4,6 +4,8 @@ import android.support.annotation.NonNull;
 
 import com.fanap.podasync.model.DeviceResult;
 import com.fanap.podchat.model.EncResponse;
+import com.fanap.podchat.model.OutPutDefineSecretKey;
+import com.fanap.podchat.model.OutPutGetKey;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -13,6 +15,8 @@ import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 import rx.Observable;
 
 public interface SSOApi {
@@ -31,5 +35,22 @@ public interface SSOApi {
             @Field("validity") long validity
     );
 
+
+    @NonNull
+    @GET("srv/encryption/keys/{keyId}")
+    Observable<Response<OutPutGetKey>> generateEncryptionPrivateKey(@Header("Authorization") String token
+            , @Header("X-Encrypt-KeyId") String secretKeyId
+            , @Path("keyId") String keyId
+            , @Query("keyFormat") String keyFormat
+         );
+
+
+    @NonNull
+    @POST("srv/encryption/keys/define")
+    @FormUrlEncoded
+    Observable<Response<OutPutDefineSecretKey>> defineSecretKey(@Header("Authorization") String token
+            , @Field("secretKey") String secretKey
+            , @Field("algorithm") String algorithm
+    );
 
 }
